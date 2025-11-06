194554658993c34046806a120ab5635ea0cceff3
peeps <101641076+halfpeeps@users.noreply.github.com>
2025-11-06 03:05:26 +0000

    Revert "Use Chakra"
    This reverts commit 81e6845a5c820b906ac0845b073f2cff91581d23.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,5 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  );
+  return <Component {...pageProps} />;
 }
