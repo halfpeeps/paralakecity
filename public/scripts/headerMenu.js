c000b6bd3244473535e8c2d6052d107abedd3c31
peeps <101641076+halfpeeps@users.noreply.github.com>
2026-04-23 21:16:52 +0100

    Remove light mode

--- public/scripts/headerMenu.js
+++ public/scripts/headerMenu.js
@@ -3,13 +3,13 @@ document.addEventListener("DOMContentLoaded", function () {
     const currentTheme = localStorage.getItem("theme") || "dark";
 
     document.documentElement.setAttribute("data-theme", currentTheme);
-    themeToggle.textContent = currentTheme === "dark" ? "Light Theme" : "Dark Theme";
+    themeToggle.textContent = currentTheme === "dark" ? "☼" : "☽";
 
     themeToggle.addEventListener("click", function () {
         let newTheme = document.documentElement.getAttribute("data-theme") === "dark" ? "light" : "dark";
 
         document.documentElement.setAttribute("data-theme", newTheme);
         localStorage.setItem("theme", newTheme);
-        themeToggle.textContent = newTheme === "dark" ? "Light Theme" : "Dark Theme";
+        themeToggle.textContent = newTheme === "dark" ? "☼" : "☽";
     });
 });
